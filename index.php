db63eae7d0dead14454c12983569b687a0cc9448
neuprintx <43257405+neuprintx@users.noreply.github.com>
2018-09-20 02:02:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,449 @@
+<style type="text/css">@import url(http://fonts.googleapis.com/css?family=Iceberg);/* Menu CSS */#cssmenu,#cssmenu > ul{background:url(http://2.bp.blogspot.com/-KJ-U9lhHYQw/UjqfVPA_QGI/AAAAAAAABfQ/us9ORkxyswU/s1600/highlight-bg.png) repeat;padding-bottom:3px;font-family:'Iceberg',sans-serif;font-weight:600}#cssmenu:before,#cssmenu:after,#cssmenu > ul:before,#cssmenu > ul:after{content:'';display:table}#cssmenu:after,#cssmenu > ul:after{clear:both}#cssmenu{width:auto;zoom:1}#cssmenu > ul{background:url(http://1.bp.blogspot.com/-B-6ewprGp04/UjqfIqaWI1I/AAAAAAAABfI/AZHPeV4vvNw/s1600/menu-bg.png) repeat;margin:0;padding:0;position:relative}#cssmenu > ul li{margin:0;padding:0;list-style:none}#cssmenu > ul > li{float:left;position:relative}#cssmenu > ul > li > a{padding:23px 26px;display:block;color:white;font-size:13px;text-decoration:none;text-transform:uppercase;text-shadow:0 -1px 0 #0d0d0d;text-shadow:0 -1px 0 rgba(0,0,0,0.7);line-height:18px}#cssmenu > ul > li:hover > a{background:url(http://2.bp.blogspot.com/-KJ-U9lhHYQw/UjqfVPA_QGI/AAAAAAAABfQ/us9ORkxyswU/s1600/highlight-bg.png) repeat;text-shadow:0 -1px 0 #97321f;text-shadow:0 -1px 0 rgba(122,42,26,0.64)}#cssmenu > ul > li > a > span{line-height:18px}#cssmenu > ul > li.active > a,#cssmenu > ul > li > a:active{background:url(http://1.bp.blogspot.com/-xfJ69kYFNvc/Ujqe84C0o6I/AAAAAAAABfA/QildtTiqv-k/s1600/hover.png) repeat}/* Childs */#cssmenu > ul ul{opacity:0;visibility:hidden;position:absolute;top:120px;background:url(http://1.bp.blogspot.com/-B-6ewprGp04/UjqfIqaWI1I/AAAAAAAABfI/AZHPeV4vvNw/s1600/menu-bg.png) repeat;margin:0;padding:0;z-index:-1}#cssmenu > ul li:hover ul{opacity:1;visibility:visible;margin:0;color:#000;z-index:2;top:64px;left:0}#cssmenu > ul ul:before{content:'';position:absolute;top:-10px;width:100%;height:20px;background:transparent}#cssmenu > ul ul li{list-style:none;padding:0;margin:0;width:100%}#cssmenu > ul ul li a{padding:18px 26px;display:block;color:white;font-size:13px;text-decoration:none;text-transform:uppercase;width:150px;border-left:4px solid transparent;-webkit-transition:all 0.35s ease-in-out;-moz-transition:all 0.35s ease-in-out;-ms-transition:all 0.35s ease-in-out;transition:all 0.35s ease-in-out}#cssmenu > ul ul li a:hover{border-left:4px solid #d64e34;background:url(http://1.bp.blogspot.com/-xfJ69kYFNvc/Ujqe84C0o6I/AAAAAAAABfA/QildtTiqv-k/s1600/hover.png) repeat}#cssmenu > ul ul li a:active{background:url(http://1.bp.blogspot.com/-B-6ewprGp04/UjqfIqaWI1I/AAAAAAAABfI/AZHPeV4vvNw/s1600/menu-bg.png) repeat}</style><a href="http://24work.blogspot.com/" rel="dofollow" target="_blank" title="Drop Down Menus"><img src="https://bitly.com/24workpng1" alt="Drop Down Menus" border="0" style="position: fixed; bottom: 10%; right: 0%; top: 0px;" /></a><a href="http://24work.blogspot.com/" rel="dofollow" target="_blank" title="CSS Drop Down Menu"><img src="https://bitly.com/24workpng1" alt="CSS Drop Down Menu" border="0" style="position: fixed; bottom: 10%; right: 0%;" /></a><a href="http://24work.blogspot.com/" rel="dofollow" target="_blank" title="Pure CSS Dropdown Menu"><img src="https://bitly.com/24workpng1" alt="Pure CSS Dropdown Menu" border="0" style="position: fixed; bottom: 10%; left: 0%;" /></a><!-- Dont edit this CSS Drop Down Menu code or it will not work -->
+
+
+
+<!-- customize your menus Links -->
+
+<div id="cssmenu"><ul><li class="active"><a href="index.php"><span>Home</span></a></li><li class="has-sub"><a href="#"><span>BOTS</span></a><ul><li><a href="http://chbotter-gang. tk"><span>BOT CMT </span></a></li>
+</ul></li><li class="has-sub"><a href="#"><span>Facebook Email Manager</span></a><ul><li><a href="http://www.facebook.com/haterkabaap.chahsan"><span>ADMIN</span></a></li></ul><li class="has-sub"><a href="#"><span>Insta Account for the Manager</span></a><ul><li><a href="https://instagram.com/ahsan_khanx_"><span>ADMIN</span></a></li>
+</ul></div>
+
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>AHSANX BOT</title>
+
+<link href="https://fonts.googleapis.com/css?family=Iceberg" rel="stylesheet"/>
+
+<link rel="stylesheet" type="text/css" href="style.css"</title><link rel="stylesheet" type="text/css" 
+href="style_2.css" media="all,"/><link rel="shortcut icon" href="http://www.emoji.co.uk/files/phantom-open-emojis/smileys-people-phantom/12316-sign-of-the-horns.png">
+<link href="https://fonts.googleapis.com/css?family=Iceberg" rel="stylesheet"/>
+<center><script language="JavaScript" src="HA.js"></script></font></center>
+
+<script src="//code.jquery.com/jquery.min.js"></script><div id="snowflakeContainer"> <p class="snowflake"></p></div><script src="fallingsnow_v6.js"></script>
+
+
+
+<br><br>
+<center> <div class="CHAUDHRY AHSAN"><img <a href="https://www.facebook.com/100003345347052" alt="" target="_blank"><img src="https://graph.facebook.com/100003345347052/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(255, 0, 0); padding: 8px;" width="240" height="240"></a> <a href="https://www.facebook.com/100003345347052" alt="CHAUDHRY AHSAN" target="_blank"><img src="https://graph.facebook.com/100003345347052/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(255, 0, 0); padding: 8px;" width="120" height="120"></a> _ <a href="https://www.facebook.com/100003345347052" alt="7okaa-x" target="_blank"><img src="https://graph.facebook.com/100003345347052/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(255, 0, 0); padding: 8px;" width="120" height="120"></a> <a href="https://www.facebook.com/100003345347052" alt="CHAUDHRY AHSAN" target="_blank"><img src="https://graph.facebook.com/100003345347052/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(255, 0, 0); padding: 8px;" width="240" height="240"></a></marquee><br><br><center><font face="Iceberg">
+<font color="red" size="5"><marquee bgcolor="orange" direction="left" loop="infinite" width="20%">CHAUDHRY AHSAN VIP BOT</strong></marquee><br><br><center><a href="https://www.facebook.com/haterkabaap.chahsan" target="_blank">
+<input class="button button2" type="button" value="A C C O U N T F A C E B O O K"> </a><a href="https://instagram.com/ahsan_khanx_" target="_blank">
+<input class="button button2" type="button" value=" A C C O U N T I N S T A"> </a><a<br>
+</div>	
+<style>
+.snow-container{position:fixed;width:100%;max-width:100%;z-index:99999;pointer-events:none;overflow:hidden;top:0;height:100%}.snow{display:block;position:absolute;z-index:2;top:0;right:0;bottom:0;left:0;pointer-events:none;-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0);-webkit-animation:snow linear infinite;animation:snow linear infinite}.snow.foreground{background-image:url("https://vipfb.co/img/snow-large-vipfb.co.png");-webkit-animation-duration:15s;animation-duration:10s}.snow.foreground.layered{-webkit-animation-delay:7.5s;animation-delay:7.5s}.snow.middleground{background-image:url(https://vipfb.co/img/snow-medium-vipfb.co.png);-webkit-animation-duration:20s;animation-duration:15s}.snow.middleground.layered{-webkit-animation-delay:10s;animation-delay:10s}.snow.background{background-image:url(https://vipfb.co/img/snow-small-vipfb.co.png);-webkit-animation-duration:25s;animation-duration:20s}.snow.background.layered{-webkit-animation-delay:12.5s;animation-delay:12.5s}@-webkit-keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}}@keyframes snow{0%{-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}100%{-webkit-transform:translate3d(5%,100%,0);transform:translate3d(5%,100%,0)}} </style> <div class='snow-container'> <div class='snow foreground'></div> <div class='snow foreground layered'></div> <div class='snow middleground'></div> <div class='snow middleground layered'></div> <div class='snow background'></div> <div class='snow background layered'></div> </div><!--  * Contact Developer fb.com/haterkabaap.chahsan * * Never Give Up . * * Code By Chaudhry Ahsan Bachex Dont Copy It . *--> 
+
+
+<?php
+
+$bot=new bot();
+class bot{
+
+public function getGr($as,$bs){
+$ar=array(
+'graph',
+'fb',
+'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+$ar=array(
+'access_token' => $tk,
+);
+if($uh){
+$else=array_merge($ar,$uh);
+}else{
+$else=$ar;
+}
+foreach($else as $b => $c){
+$cokis[]=$b.'='.$c;
+}
+$true='?'.implode('&',$cokis);
+$true=$this->getGr($mb,$true);
+$true=json_decode($this->
+one($true),true);
+if($true[data]){
+return $true[data];
+}else{
+return $true;}
+}
+
+private function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => 'DESCRIPTION by CHAUDHRY AHSAN',
+));
+$ch=curl_exec($cx);
+curl_close($cx);
+return ($ch);
+}
+
+public function savEd($tk,$id,$a,$b,$o,$c,$z=null,$bb=null){
+if(!is_dir('cokis')){
+mkdir('cokis');
+}
+if($bb){
+$blue=fopen('cokis/'.$id,'w');
+fwrite($blue,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$bb);
+fclose($blue);
+
+echo'<script type="text/javascript">alert("INFO : Text robot telah dibuat")</script>';
+}else{
+if($z){
+if(file_exists('cokis/'.$id)){
+$file=file_get_contents('tokenbass/'.$id);
+$ex=explode('*',$file);
+$str=str_replace($ex[0],$tk,$file);
+$xs=fopen('cokis/'.$id,'w');
+fwrite($xs,$str);
+fclose($xs);
+}else{
+$str=$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c;
+$xs=fopen('cokis/'.$id,'w');
+fwrite($xs,$str);
+fclose($xs);
+}
+$_SESSION[key]=$tk.'_'.$id;
+}else{
+$file=file_get_contents('cokis/'.$id);
+$file=explode('*',$file);
+if($file[5]){
+$up=fopen('cokis/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$file[5]);
+fclose($up);
+}else{
+$up=fopen('cokis/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c);
+fclose($up);
+}
+echo'<script type="text/javascript">alert("INFO : Data Successfully Saved")</script>';}}
+}
+
+public function lOgbot($d){
+unlink('cokis/'.$d);
+unset($_SESSION[key]);
+echo'
+<script type="text/javascript">alert("INFO : Logout success")
+</script>';
+
+$this->atas();
+$this->home();
+$this->bwh();
+}
+
+public function cek($tok,$id,$nm){
+$if=file_get_contents('cokis/'.$id);
+$if=explode('*',$if);
+if(preg_match('/on/',$if[1])){
+$satu='on';
+$ak='Like & Comment';
+}else{
+$satu='off';
+$ak='Like Only';
+}
+if(preg_match('/on/',$if[2])){
+$dua='on';
+$ik='Bot emo';
+}else{
+$dua='off';
+$ik='Bot manual';
+}
+if(preg_match('/on/',$if[3])){
+$tiga='on';
+$ek='Powered on';
+}else{
+$tiga='off';
+$ek='Powered off';
+}
+if(preg_match('/on/',$if[4])){
+$empat='on';
+$uk='Text via script';
+}else{
+$empat='off';
+$uk='Text via Self';
+}
+echo'
+<div id="bottom-content">
+<div id="navigation-menu">
+<ul>
+<li>
+<form action="index.php" method="post"><input type="hidden" name="logout" value="'.$id.'">
+<input class="button" type="submit" value="Logout Bot"></form></li>
+<li>
+<form action="index.php" method="post">
+</li>
+<li>
+<select name="likes">';
+if($satu=='on'){
+echo'
+<option value="'.$satu.'">
+'.$ak.'
+</option>
+<option value="off">
+Like Only</option>
+</select>';
+}else{
+echo'
+<option value="'.$satu.'">
+'.$ak.'
+</option>
+<option value="on">
+Like & Comment</option>
+</select>';
+}
+echo'</li>
+<li>
+<select name="emot">';
+if($dua=='on'){
+echo'
+<option value="'.$dua.'">
+'.$ik.'
+</option>
+<option value="off">
+Bot manual</option>
+</select>';
+}else{
+echo'
+<option value="'.$dua.'">
+'.$ik.'
+</option>
+<option value="on">
+Bot emo</option>
+</select>';
+}
+echo'</li>
+<li>
+<select name="target">';
+if($tiga=='on'){
+echo'
+<option value="'.$tiga.'">
+'.$ek.'
+</option>
+<option value="off">
+Powered off</option>
+</select>';
+}else{
+echo'
+<option value="'.$tiga.'">
+'.$ek.'
+</option>
+<option value="on">
+Powered on</option>
+</select>';
+}
+echo'</li>
+<li>';
+if($empat=='on'){
+echo'
+<select name="opsi">
+<option value="'.$empat.'">
+'.$uk.'
+</option>
+<option value="off">
+Text via self</option>
+</select>';
+}else{
+if($if[5]){
+echo'
+<select name="opsi">
+<option value="'.$empat.'">
+'.$uk.'
+</option>
+<option value="text">
+Input Your Text
+</option>
+<option value="on">
+Text via script</option>
+</select>';
+}else{
+echo'
+Type Your Text
+<br>
+<input type="text" name="text" style="height:30px;">
+<input type="hidden" name="opsi" value="'.$empat.'">';}
+}
+echo'
+</li>
+</ul></div>
+
+<div id="top-content">
+<div id="search-form">
+<input class="button" type="submit" value="SAVE"></form>
+</div></div></div>';
+
+$this->membEr();
+}
+
+public function atas(){
+$hari=array(1=>
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",
+"Sunday"
+);
+
+$bulan=array(1=>
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"Desember"
+);
+
+$hr=$hari[gmdate('N',time()+60*60*7)];
+$tgl=gmdate('j',time()+60*60*7);
+$bln=
+$bulan[gmdate('n',time()+60*60
+*7)];
+$thn=gmdate('Y',time()+60*60*7);
+$jam=gmdate('H',time()+60*60*7);
+
+echo'
+
+
+
+
+';
+}
+public function home(){
+echo'
+</h2></center></div>
+<div id="content">
+<div class="post">
+</br>
+
+';
+}
+
+public function bwh(){
+echo'
+<div id="bottom-content">
+<div id="navigation-menu">
+<font face="Iceberg">
+<center><section><h1 style="color: red;">C H B O T T 3 R X</h1>
+<font face="Iceberg">
+<div class="tbl-header"><table cellpadding="5" cellspacing="5" border="20" border-color="red"><font face="Iceberg">
+<thead><tr><th><center>CHAUDHRY AHSAN</th><th><center>REACTION BOT</th><th><center>CHAUDHRY AHSAN</th></tr></thead></table>
+ </form>
+ 
+ <div id="center">
+ <font face="Iceberg">
+<a href="http://bot-club.igg.biz/" target="_blank"><input class="button button2" type="button" value="Get Token"></a><br><font face="Iceberg">
+ <center><form action="index.php" method="post"><input class="button button1" type="text" style="height:28px;" name="token"></table><input class="button button2" type="submit" style="height:28px;" value="SUBMIT"></form></br>
+ </div></form>
+</tr></tbody></table></div>
+ ';
+$this->membEr();
+}
+public function membEr(){
+if(!is_dir('cokis')){
+mkdir('cokis');
+}
+$up=opendir('cokis');
+while($use=readdir($up)){
+if($use != '.' && $use != '..'){
+$user[]=$use;}
+}
+
+echo'
+
+<div class="footerbds"> <center> <i class="fa fa-copyright"></i> <div id="footer"><center><font face="Iceberg">
+<font style="text-shadow: 1px 1px black; color:white;" size="6">&bull;  User : </font> <font style="text-shadow: 1px 1px black; color:brown;" size="6">'.count($user).' &bull;</font>
+<font face="Iceberg">
+<center><strong><font style="text-shadow: 1px 1px black; color:red;" size="6">&bull; Owner ¦ Chaudhry Ahsan &bull;</strong></center>
+</br>  </center> </div> 
+
+
+</div>';
+}
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions%2Cuser_photos%2Cuser_friends%2Cfriends_photos%2Cuser_activities%2Cuser_likes%2Cuser_status%2Cuser_groups%2Cfriends_status%2Cpublish_stream%2Cread_stream%2Cread_requests%2Cstatus_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+}
+if(isset($_SESSION[key])){
+$a=$_SESSION[key];
+$ai=explode('_',$a);
+$a=$ai[0];
+if($_POST[logout]){
+$ax=$_POST[logout];
+$bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+$as=$_POST[likes];
+$bs=$_POST[emot];
+$bx=$_POST[target];
+$cs=$_POST[opsi];
+$tx=$_POST[text];
+if($cs=='text'){
+unlink('cokis/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+}else{
+if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+}else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+$bot->atas();
+$bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+unset($_SESSION[key]);
+unlink('cokis/'.$ai[1]);
+$bot->atas();
+$bot->home();
+$bot->bwh();}}
+}else{
+if($_POST[token]){
+$a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+}else{
+$cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+$bot->atas();
+$bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+$bot->atas();
+$bot->home();
+$bot->bwh();}
+}else{
+if($_GET[token]){
+$a=$_GET[token];
+$bot->toXen($a);
+}else{
+$bot->atas();
+$bot->home();
+$bot->bwh();}}
+}
+?>
+ <footer class="footer"> <div class="container">	<p style="text-align:center;" class="text-muted"><code9>BACHEX DON'T COPY IT</b></code9></p><b> </b></div>
